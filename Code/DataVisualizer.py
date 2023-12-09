54c1ec5539eb535030642118065ed9e048d2391a
Nicholas <englishnicholas@yahoo.com>
2023-12-09 15:37:05 -0500

    Added titles tp decision tree graphs [phase-6]

--- Code/DataVisualizer.py
+++ Code/DataVisualizer.py
@@ -20,5 +20,6 @@ def DecisionTree(columns:[str], model:tree.DecisionTreeClassifier, caseType:str)
 
     plt.figure(figsize=(40, 10))
     tree.plot_tree(model, feature_names=columns, class_names=model.classes_, filled=True, rounded=True)
+    plt.suptitle(caseType, fontsize=40)
     plt.savefig(f'{GlobalConfigs.DECISION_TREE_GRAPHS_FILEPATH}{caseType}.png', dpi=600)
     plt.close()
